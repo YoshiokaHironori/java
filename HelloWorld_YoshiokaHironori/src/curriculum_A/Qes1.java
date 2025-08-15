078f8bffab4bfe168f7e344be8b08b4c401b2c40
PC_User <PC_User@DESKTOP-F9SVESI>
2025-08-15 20:02:07 +0900

    課題１修正版　吉岡宏倫

--- HelloWorld_YoshiokaHironori/src/curriculum_A/Qes1.java
+++ HelloWorld_YoshiokaHironori/src/curriculum_A/Qes1.java
@@ -3,32 +3,32 @@ package curriculum_A;
 public class Qes1 {
 
 	public static void main(String[] args) {
-		byte x;
-	    short d ;
-		int aa;	
-		long a;
-		float b;
-		double c;
-		char letter;
-		String letters;
-		boolean is;
+		byte byt=0;
+	    short sht=0 ;
+		int number=0;	
+		long lon=0L;
+		float flt=0.0f;
+		double dbl=0.0;
+		char letter='\u0000';
+		String letters=null;
+		boolean is=false;
 		
-		x = 10;
-		d = 100;
-		aa = 1000;
-		a = 10000;
-		b = 9.5f;
-		c = 10.5;
+		byt = 10;
+		sht = 100;
+		number = 1000;
+		lon = 10000;
+		flt = 9.5f;
+		dbl = 10.5;
 		letter = 'a';
 		letters = "ハロー";
 		is = true;
-		System.out.println(x+d+aa+a);
-		System.out.println(x+x);
+		System.out.println(byt+sht+number+lon);
+		System.out.println(byt+byt);
 		System.out.println(letter+letters+is);
-		System.out.println(x+d+aa+a+b+c);
-		System.out.println(x*d*aa*a);
-		System.out.println(c/d);
-		System.out.println(x-d);
+		System.out.println(byt+sht+number+lon+flt+dbl);
+		System.out.println(byt*sht*number*lon);
+		System.out.println(dbl/sht);
+		System.out.println(byt-sht);
 		
 		String name ="山田太郎";
 		String e ="こんにちは";
@@ -52,20 +52,21 @@ public class Qes1 {
 		System.out.println("整数価格:"+bb);
 		
 		String numStr = "123";
-		int cc =Integer.parseInt(numStr+10);
-		System.out.println("変換後の値:"+cc);
+		int cc =Integer.parseInt(numStr);
+		int num3 =cc+10;
+		System.out.println("変換後の値:"+num3);
 		
 		int num = 50;
 		String dd = Integer.toString(num);
 		System.out.println("得点:"+dd);
 		
-		int aaa = 10;
-		int bbb = 20;
-		boolean result =aaa<bbb;
+		int num5 = 10;
+		int num4 = 20;
+		boolean result =num5<num4;
 		System.out.println(result);
 		
-		int xx = 15;
-		String q =(xx>10)?"OK":"NG" ;
+		int num6 = 15;
+		String q =(num6>10)?"OK":"NG" ;
 		System.out.println(q);
 		
 		String text ="私はJavaが好きです。Javaは楽しい！";
